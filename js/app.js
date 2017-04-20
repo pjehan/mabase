6f5ec3407958f25cc2c8294b2608dbe717922613
pjehan <pierre.jehan@gmail.com>
2017-04-20 14:45:45 +0200

    Insert Personne

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
 
-    var serverIP = "10.1.106.4";
+    var serverIP = "localhost";
     var serverPort = "8080";
     var serverURL = "http://" + serverIP + ":" + serverPort;
 
     // DOM Elements
     var $personnes = $("#personnes tbody");
+    var $formPersonne = $("#form-personne");
 
     $.ajax({
         method: "GET",
@@ -21,6 +22,28 @@ $(document).ready(function() {
             tr.find("td.firstname").text(personne.firstname);
             tr.find("td.lastname").text(personne.lastname);
             $personnes.append(tr);
+        });
+    });
+
+    $formPersonne.submit(function(event) {
+        event.preventDefault();
+
+        var formMethod = $(this).attr("method");
+        var formAction = $(this).attr("action");
+
+        var firstname = $(this).find("input[name=firstname]").val();
+        var lastname = $(this).find("input[name=lastname]").val();
+
+        $.ajax({
+            method: formMethod,
+            url: serverURL + formAction,
+            data: {
+                firstname: firstname,
+                lastname: lastname
+            }
+        })
+        .done(function(){
+
         });
     });
 
