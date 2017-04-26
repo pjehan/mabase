7083e957d262694c1a98cffc5b790ac196a61015
pjehan <pierre.jehan@gmail.com>
2017-04-26 22:49:19 +0200

    Bootstrap with modal

--- js/app.js
+++ js/app.js
@@ -1,23 +1,29 @@
 $(document).ready(function() {
 
+    // Server config
     var serverIP = "localhost";
     var serverPort = "8080";
     var serverURL = "http://" + serverIP + ":" + serverPort;
 
     // DOM Elements
     var $personnes = $("#personnes tbody");
-    var $formPersonne = $("#form-personne");
+    var $btnNewPerson = $("#btn-new-person");
+    var $modalFormPerson = $('#modal-personne-form');
+    var $editRow = null;
 
+    // Insert new line in table
     function newPersonne(personne) {
         var tr = $personnes.find("tr.hidden").clone();
         tr.removeClass("hidden");
         tr.find("td.firstname").text(personne.firstname);
         tr.find("td.lastname").text(personne.lastname);
         tr.find("form").attr("action", serverURL + "/personne/" + personne.id);
+        tr.find("a.edit-personne").attr("href", serverURL + "/personne/" + personne.id);
         $personnes.prepend(tr);
         tr.hide().fadeIn();
     }
 
+    // Load data on start
     $.ajax({
         method: "GET",
         url: serverURL + "/personne"
@@ -28,7 +34,57 @@ $(document).ready(function() {
         });
     });
 
-    $formPersonne.submit(function(event) {
+    // Click button add person
+    $btnNewPerson.click(function(event){
+        event.preventDefault();
+        $modalFormPerson.find(".modal-title").text("Nouvelle personne");
+        $modalFormPerson.find("form").attr("action", "/personne");
+        $modalFormPerson.find("form").attr("method", "POST");
+        $modalFormPerson.find("form")[0].reset();
+        $modalFormPerson.modal("show");
+    });
+
+    // Click button delete person
+    $personnes.on("submit", "form", function(event){
+        event.preventDefault();
+
+        var self = this;
+
+        var formMethod = $(this).attr("method");
+        var formAction = $(this).attr("action");
+
+        $.ajax({
+            method: formMethod,
+            url: formAction
+        })
+        .done(function(){
+            $(self).closest("tr").fadeOut();
+        });
+    });
+
+    // Click button edit person
+    $personnes.on("click", "a.edit-personne", function(event) {
+        event.preventDefault();
+
+        var href = $(this).attr("href");
+        $editRow = $(this).closest("tr");
+
+        $.ajax({
+            method: "GET",
+            url: href
+        })
+        .done(function(personne) {
+            $modalFormPerson.find(".modal-title").text("Modifier personne");
+            $modalFormPerson.find("form").attr("action", "/personne/" + personne.id);
+            $modalFormPerson.find("form").attr("method", "PUT");
+            $modalFormPerson.find("form input[name=firstname]").val(personne.firstname);
+            $modalFormPerson.find("form input[name=lastname]").val(personne.lastname);
+            $modalFormPerson.modal("show");
+        });
+    });
+
+    // Submit edit or create person form
+    $modalFormPerson.find("form").submit(function(event) {
         event.preventDefault();
 
         var self = this;
@@ -48,25 +104,16 @@ $(document).ready(function() {
             }
         })
         .done(function(personne){
-            newPersonne(personne);
+            if (formMethod == "POST") {
+                newPersonne(personne);
+            } else {
+                $editRow.find(".firstname").text(personne.firstname);
+                $editRow.find(".lastname").text(personne.lastname);
+                $editRow.addClass("animated flipInX");
+                setTimeout(function(){$editRow.removeClass("animated flipInX")}, 2000);
+            }
             self.reset();
-        });
-    });
-
-    $personnes.on("submit", "form", function(event){
-        event.preventDefault();
-
-        var self = this;
-
-        var formMethod = $(this).attr("method");
-        var formAction = $(this).attr("action");
-
-        $.ajax({
-            method: formMethod,
-            url: formAction
-        })
-        .done(function(){
-            $(self).closest("tr").fadeOut();
+            $modalFormPerson.modal("hide");
         });
     });
 
